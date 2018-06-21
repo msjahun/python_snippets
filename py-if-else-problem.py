4c18a96f1c9592d7de3b93a5e66a43e8c4f3acad
Musa Jahun <msjahun@live.com>
2018-06-21 14:05:56 +0300

    added question

--- py-if-else-problem.py
+++ py-if-else-problem.py
@@ -1,4 +1,17 @@
 #!/bin/python3
+#     
+#     py-if-else-problem
+# Task Given an integer, , perform the following conditional actions:
+# 
+# If is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than , print Not Weird Input Format
+# 
+# A single line containing a positive integer, .
+# 
+# Constraints
+# 
+# Output Format
+# 
+# Print Weird if the number is weird; otherwise, print Not Weird.
 
 import math
 import os
